879c10989dcc698bad2a1b812fab18d949077497
Minh Chiến <65323942+tranchien021@users.noreply.github.com>
2023-07-25 12:10:18 +0700

    Update get_data_home.php

--- get_data_home.php
+++ get_data_home.php
@@ -1,6 +1,6 @@
 <?php
 include('simple_html_dom.php');
-$url = 'https://vnexpress.net/rss/tin-moi-nhat.rss';
+$url = 'https://dantri.com.vn/giao-duc/vu-lo-de-thi-sinh-8-thi-sinh-duoc-mom-de-can-xu-ly-the-nao-20230620004656478.htm';
 $lines_array = file($url);
 $lines_string = implode('', $lines_array);
 $xml = simplexml_load_string($lines_string);
